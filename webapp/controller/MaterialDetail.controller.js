45cb2697638cbc0e4318be08f7a7652392cc94a8
Gurkan Yilmaz <yilmaz.gurkan@hotmail.com.tr>
2019-05-22 11:54:31 +0300

    read entity

--- webapp/controller/MaterialDetail.controller.js
+++ webapp/controller/MaterialDetail.controller.js
@@ -1,240 +1,236 @@
 sap.ui.define(
-  [
-    "gqsystema/zmaterial_man/controller/BaseController",
-    "gqsystema/zmaterial_man/util/BusyIndicator",
-    "sap/m/MessageToast",
-    "gqsystema/zmaterial_man/util/BackendService",
-    "gqsystema/zmaterial_man/util/Constants"
-  ],
-  function(
-    BaseController,
-    BusyIndicator,
-    MessageToast,
-    BackendService,
-    Constants
-  ) {
-    "use strict";
-    var that;
-    return BaseController.extend(
-      "gqsystema.zmaterial_man.controller.MaterialDetail",
-      {
-        onInit: function() {
-          that = this;
+	[
+		"gqsystema/zmaterial_man/controller/BaseController",
+		"gqsystema/zmaterial_man/util/BusyIndicator",
+		"sap/m/MessageToast",
+		"gqsystema/zmaterial_man/util/BackendService",
+		"gqsystema/zmaterial_man/util/Constants"
+	],
+	function(
+		BaseController,
+		BusyIndicator,
+		MessageToast,
+		BackendService,
+		Constants
+	) {
+		"use strict";
+		var that;
+		return BaseController.extend(
+			"gqsystema.zmaterial_man.controller.MaterialDetail", {
+				onInit: function() {
+					that = this;
+					that.matModel = this.getModel(Constants.MATERIAL_MODEL);
+					that.backendService = new BackendService(that.matModel);
+				},
+				onSearch: function() {
+					var val = this.getView()
+						.byId("scannedValue")
+						.getProperty("value");
+					if (val) {
+						var oBusyIndicator = new BusyIndicator();
+						oBusyIndicator.show();
+						var reqUrl = Constants.MATERIAL_SET + "('" + val + "')";
+						debugger;
+						that.backendService
+							.readEntity(reqUrl, null, null)
+							.then(
+								function(data, fullfil) {
+									debugger;
+									this.getModel(Constants.MATERIAL_MODEL).setData(data);
+								}.bind(this)
+							)
+							.catch(
 
-          that.backendService = new BackendService("materialModel");
-        },
-        onSearch: function() {
-          var val = this.getView()
-            .byId("scannedValue")
-            .getProperty("value");
-          if (val) {
-            var oBusyIndicator = new BusyIndicator();
-            oBusyIndicator.show();
-            var reqUrl = Constants.MaterialSet + "('" + val + "')";
-            debugger;
-            that.backendLogic
-              .readEntity(reqUrl, null, null)
-              .then(
-                function(data, fullfil) {
-                  this.getModel(Constants.MATERIAL_MODEL).setData(data);
-                }.bind(this)
-              )
-              .catch(
-                function(error) {
-                  MessageBox.error(error, {
-                    title: this.getResourceBundle().getText(
-                      "General.BackendErrorOccured.title"
-                    ),
-                    styleClass: this.getStyleClass(),
-                    actions: [MessageBox.Action.CLOSE]
-                  });
-                  // create the views based on the url/hash
-                  //this line is moved here in order to reassure that the customizing data is retrieved for the backend as it is necessary for all screens.
-                  //this approach is chosen in order to avoid retrieving the same data in each controller
-                  this.getRouter().initialize();
-                }.bind(this)
-              );
-          } else {
-            var msg = this.getModel("i18n")
-              .getResourceBundle()
-              .getText("emptyMaterialNumber");
-            MessageToast.show(msg);
-          }
-        },
-        onScanForValue: function(oEvent) {
-          if (!this._oScanDialog) {
-            this._oScanDialog = new sap.m.Dialog({
-              title: "Scan barcode",
-              contentWidth: "640px",
-              contentHeight: "480px",
-              horizontalScrolling: false,
-              verticalScrolling: false,
-              stretchOnPhone: true,
-              content: [
-                new sap.ui.core.HTML({
-                  id: this.createId("scanContainer"),
-                  content: "<div />"
-                })
-              ],
-              endButton: new sap.m.Button({
-                text: "Cancel",
-                press: function(oEvent) {
-                  this._oScanDialog.close();
-                }.bind(this)
-              }),
-              afterOpen: function() {
-                this._initQuagga(
-                  this.getView()
-                    .byId("scanContainer")
-                    .getDomRef()
-                )
-                  .done(function() {
-                    // Initialisation done, start Quagga
-                    Quagga.start();
-                  })
-                  .fail(
-                    function(oError) {
-                      MessageToast.error(
-                        oError.message.length
-                          ? oError.message
-                          : "Failed to initialise Quagga with reason code " +
-                              oError.name,
-                        {
-                          onClose: function() {
-                            this._oScanDialog.close();
-                          }.bind(this)
-                        }
-                      );
-                    }.bind(this)
-                  );
-              }.bind(this),
-              afterClose: function() {
-                // Dialog closed, stop Quagga
-                Quagga.stop();
-              }
-            });
+							);
+					} else {
+						var msg = this.getModel("i18n")
+							.getResourceBundle()
+							.getText("emptyMaterialNumber");
+						MessageToast.show(msg);
+					}
+				},
+				onScanForValue: function(oEvent) {
+					if (!this._oScanDialog) {
+						this._oScanDialog = new sap.m.Dialog({
+							title: "Scan barcode",
+							contentWidth: "640px",
+							contentHeight: "480px",
+							horizontalScrolling: false,
+							verticalScrolling: false,
+							stretchOnPhone: true,
+							content: [
+								new sap.ui.core.HTML({
+									id: this.createId("scanContainer"),
+									content: "<div />"
+								})
+							],
+							endButton: new sap.m.Button({
+								text: "Cancel",
+								press: function(oEvent) {
+									this._oScanDialog.close();
+								}.bind(this)
+							}),
+							afterOpen: function() {
+								this._initQuagga(
+										this.getView()
+										.byId("scanContainer")
+										.getDomRef()
+									)
+									.done(function() {
+										// Initialisation done, start Quagga
+										Quagga.start();
+									})
+									.fail(
+										function(oError) {
+											MessageToast.error(
+												oError.message.length ? oError.message : "Failed to initialise Quagga with reason code " +
+												oError.name, {
+													onClose: function() {
+														this._oScanDialog.close();
+													}.bind(this)
+												}
+											);
+										}.bind(this)
+									);
+							}.bind(this),
+							afterClose: function() {
+								// Dialog closed, stop Quagga
+								Quagga.stop();
+							}
+						});
 
-            this.getView().addDependent(this._oScanDialog);
-          }
+						this.getView().addDependent(this._oScanDialog);
+					}
 
-          this._oScanDialog.open();
-        },
+					this._oScanDialog.open();
+				},
 
-        _initQuagga: function(oTarget) {
-          var oDeferred = jQuery.Deferred();
-          Quagga.init(
-            {
-              inputStream: {
-                type: "LiveStream",
-                target: oTarget,
-                constraints: {
-                  width: {
-                    min: 640
-                  },
-                  height: {
-                    min: 480
-                  },
-                  facingMode: "environment"
-                }
-              },
-              locator: {
-                patchSize: "medium",
-                halfSample: true
-              },
-              numOfWorkers: 2,
-              frequency: 10,
-              decoder: {
-                readers: [
-                  "code_128_reader",
-                  "ean_reader",
-                  "ean_8_reader",
-                  "code_39_reader",
-                  "upc_reader"
-                ]
-              },
-              locate: true
-            },
-            function(error) {
-              if (error) {
-                oDeferred.reject(error);
-              } else {
-                oDeferred.resolve();
-              }
-            }
-          );
+				_initQuagga: function(oTarget) {
+					var oDeferred = jQuery.Deferred();
+					Quagga.init({
+							inputStream: {
+								type: "LiveStream",
+								target: oTarget,
+								constraints: {
+									width: {
+										min: 640
+									},
+									height: {
+										min: 480
+									},
+									facingMode: "environment"
+								}
+							},
+							locator: {
+								patchSize: "medium",
+								halfSample: true
+							},
+							numOfWorkers: 2,
+							frequency: 10,
+							decoder: {
+								readers: [
+									"code_128_reader",
+									"ean_reader",
+									"ean_8_reader",
+									"code_39_reader",
+									"upc_reader"
+								]
+							},
+							locate: true
+						},
+						function(error) {
+							if (error) {
+								oDeferred.reject(error);
+							} else {
+								oDeferred.resolve();
+							}
+						}
+					);
 
-          if (!this._oQuaggaEventHandlersAttached) {
-            // Attach event handlers...
-            Quagga.onProcessed(
-              function(result) {
-                const drawingCtx = Quagga.canvas.ctx.overlay;
-                const drawingCanvas = Quagga.canvas.dom.overlay;
-                // drawingCanvas.style.display = 'none';
+					if (!this._oQuaggaEventHandlersAttached) {
+						// Attach event handlers...
+						Quagga.onProcessed(
+							function(result) {
+								const drawingCtx = Quagga.canvas.ctx.overlay;
+								const drawingCanvas = Quagga.canvas.dom.overlay;
+								// drawingCanvas.style.display = 'none';
 
-                if (result) {
-                  if (result.boxes) {
-                    drawingCtx.clearRect(
-                      0,
-                      0,
-                      parseInt(drawingCanvas.getAttribute("width"), 10),
-                      parseInt(drawingCanvas.getAttribute("height"), 10)
-                    );
-                    result.boxes
-                      .filter(box => {
-                        return box !== result.box;
-                      })
-                      .forEach(box => {
-                        Quagga.ImageDebug.drawPath(
-                          box,
-                          { x: 0, y: 1 },
-                          drawingCtx,
-                          { color: "green", lineWidth: 2 }
-                        );
-                      });
-                  }
+								if (result) {
+									if (result.boxes) {
+										drawingCtx.clearRect(
+											0,
+											0,
+											parseInt(drawingCanvas.getAttribute("width"), 10),
+											parseInt(drawingCanvas.getAttribute("height"), 10)
+										);
+										result.boxes
+											.filter(box => {
+												return box !== result.box;
+											})
+											.forEach(box => {
+												Quagga.ImageDebug.drawPath(
+													box, {
+														x: 0,
+														y: 1
+													},
+													drawingCtx, {
+														color: "green",
+														lineWidth: 2
+													}
+												);
+											});
+									}
 
-                  if (result.box) {
-                    Quagga.ImageDebug.drawPath(
-                      result.box,
-                      { x: 0, y: 1 },
-                      drawingCtx,
-                      { color: "#00F", lineWidth: 2 }
-                    );
-                  }
+									if (result.box) {
+										Quagga.ImageDebug.drawPath(
+											result.box, {
+												x: 0,
+												y: 1
+											},
+											drawingCtx, {
+												color: "#00F",
+												lineWidth: 2
+											}
+										);
+									}
 
-                  if (result.codeResult && result.codeResult.code) {
-                    Quagga.ImageDebug.drawPath(
-                      result.line,
-                      { x: "x", y: "y" },
-                      drawingCtx,
-                      { color: "red", lineWidth: 3 }
-                    );
-                  }
-                }
-              }.bind(this)
-            );
-            Quagga.onDetected(
-              function(result) {
-                // Barcode has been detected, value will be in result.codeResult.code. If requierd, validations can be done
-                // on result.codeResult.code to ensure the correct format/type of barcode value has been picked up
+									if (result.codeResult && result.codeResult.code) {
+										Quagga.ImageDebug.drawPath(
+											result.line, {
+												x: "x",
+												y: "y"
+											},
+											drawingCtx, {
+												color: "red",
+												lineWidth: 3
+											}
+										);
+									}
+								}
+							}.bind(this)
+						);
+						Quagga.onDetected(
+							function(result) {
+								// Barcode has been detected, value will be in result.codeResult.code. If requierd, validations can be done
+								// on result.codeResult.code to ensure the correct format/type of barcode value has been picked up
 
-                // Set barcode value in input field
-                this.getView()
-                  .byId("scannedValue")
-                  .setValue(result.codeResult.code);
+								// Set barcode value in input field
+								this.getView()
+									.byId("scannedValue")
+									.setValue(result.codeResult.code);
 
-                // Close dialog
-                this._oScanDialog.close();
-              }.bind(this)
-            );
+								// Close dialog
+								this._oScanDialog.close();
+							}.bind(this)
+						);
 
-            // Set flag so that event handlers are only attached once...
-            this._oQuaggaEventHandlersAttached = true;
-          }
+						// Set flag so that event handlers are only attached once...
+						this._oQuaggaEventHandlersAttached = true;
+					}
 
-          return oDeferred.promise();
-        }
-      }
-    );
-  }
+					return oDeferred.promise();
+				}
+			}
+		);
+	}
 );
--- webapp/util/BackendService.js
+++ webapp/util/BackendService.js
@@ -1,54 +1,55 @@
-sap.ui.define([
-	"sap/ui/base/Object"
-], function(UI5Object) {
-	"use strict";
+sap.ui.define(["sap/ui/base/Object"], function(UI5Object) {
+  "use strict";
 
-	return UI5Object.extend("gqsystema.zmaterial_man.util.BackendService", {
+  return UI5Object.extend("gqsystema.zmaterial_man.util.BackendService", {
+    constructor: function(model) {
+      this._model = model;
+    },
 
-		constructor: function(model) {
-			this._model = model;
-		},
+    /**
+     *This is a generic method for reading entity set data from the backend.
+     * @param {String} entityName - the name of the entity of the set we want to retrieve from the backend
+     * @param {String} filter - a filter that might be necessary for the execution of the call
+     * @return {Object} - the result of the read method
+     */
+    readEntity: function(entityName, filter, urlParameters) {
+      return new Promise(
+        function(resolve, reject) {
+          debugger;
+          var entitySet = entityName;
+          this._model.read(entitySet, {
+            urlParameters: urlParameters,
+            filters: filter,
+            success: function(data, response) {
+              resolve(data, response);
+            },
+            error: function(error) {
+              reject(error);
+            }
+          });
+        }.bind(this)
+      );
+    },
 
-		/**
-		 *This is a generic method for reading entity set data from the backend.
-		 * @param {String} entityName - the name of the entity of the set we want to retrieve from the backend
-		 * @param {String} filter - a filter that might be necessary for the execution of the call
-		 * @return {Object} - the result of the read method
-		 */
-		readEntity: function(entityName, filter, urlParameters) {
-			return new Promise(function(resolve, reject) {
-				var entitySet = entityName;
-				this._model.read(entitySet, {
-					urlParameters: urlParameters,
-					filters: filter,
-					success: function(data, response) {
-						resolve(data, response);
-					},
-					error: function(error) {
-						reject(error);
-					}
-				});
-			}.bind(this));
-		},
-
-		/**
-		 *This is a generic method for creating a new entry in an entity set
-		 * @param {String} entitySet - the name of the entity for which we would like to create a new entry
-		 * @param {Object} entryObject - the object that represents the metadata + the values for the new entry
-		 * @return {Object} - the result of the create method
-		 */
-		createEntity: function(entitySet, entryObject) {
-			return new Promise(function(resolve, reject) {
-				this._model.create(entitySet, entryObject, {
-					success: function(data, response) {
-						resolve(data, response);
-					},
-					error: function(error) {
-						reject(error);
-					}
-				});
-			}.bind(this));
-		}
-
-	});
+    /**
+     *This is a generic method for creating a new entry in an entity set
+     * @param {String} entitySet - the name of the entity for which we would like to create a new entry
+     * @param {Object} entryObject - the object that represents the metadata + the values for the new entry
+     * @return {Object} - the result of the create method
+     */
+    createEntity: function(entitySet, entryObject) {
+      return new Promise(
+        function(resolve, reject) {
+          this._model.create(entitySet, entryObject, {
+            success: function(data, response) {
+              resolve(data, response);
+            },
+            error: function(error) {
+              reject(error);
+            }
+          });
+        }.bind(this)
+      );
+    }
+  });
 });
