13a1c2bae87cb956dc94bc9326d59768cd80882d
Gurkan Yilmaz <yilmaz.gurkan@hotmail.com.tr>
2019-05-22 20:04:19 +0300

    BusyModel

--- webapp/model/models.js
+++ webapp/model/models.js
@@ -1,16 +1,23 @@
 sap.ui.define([
 	"sap/ui/model/json/JSONModel",
 	"sap/ui/Device"
-], function (JSONModel, Device) {
+], function(JSONModel, Device) {
 	"use strict";
 
 	return {
 
-		createDeviceModel: function () {
+		createDeviceModel: function() {
 			var oModel = new JSONModel(Device);
 			oModel.setDefaultBindingMode("OneWay");
 			return oModel;
+		},
+		createHelperModel: function() {
+			var helper = {
+				isLoading: false
+			};
+			var oModel = new JSONModel(helper);
+			oModel.setDefaultBindingMode("TwoWay");
+			return oModel;
 		}
-
 	};
 });
--- webapp/util/Constants.js
+++ webapp/util/Constants.js
@@ -1,7 +1,8 @@
 sap.ui.define([], function() {
-  "use strict";
-  return Object.freeze({
-    MATERIAL_SET: "/MaterialSet",
-    MATERIAL_MODEL: ""
-  });
+	"use strict";
+	return Object.freeze({
+		MATERIAL_SET: "/MaterialSet",
+		MATERIAL_MODEL: "",
+		HELPER_MODEL: "helperModel"
+	});
 });
