8e92cf8ac8bfefc64eae1b13adbec770b380ce4a
Gurkan Yilmaz <yilmaz.gurkan@hotmail.com.tr>
2019-05-28 20:47:24 +0300

    BusyDialog

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,41 +1,40 @@
 sap.ui.define(
-	[
-		"sap/ui/core/UIComponent",
-		"sap/ui/Device",
-		"gqsystema/zmaterial_man/model/models",
-		"gqsystema/zmaterial_man/util/Constants"
-	],
-	function(UIComponent, Device, models, Constants) {
-		"use strict";
+  [
+    "sap/ui/core/UIComponent",
+    "sap/ui/Device",
+    "gqsystema/zmaterial_man/model/models",
+    "gqsystema/zmaterial_man/util/Constants"
+  ],
+  function(UIComponent, Device, models, Constants) {
+    "use strict";
 
-		return UIComponent.extend("gqsystema.zmaterial_man.Component", {
-			metadata: {
-				manifest: "json"
-			},
+    return UIComponent.extend("gqsystema.zmaterial_man.Component", {
+      metadata: {
+        manifest: "json"
+      },
 
-			/**
-			 * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
-			 * @public
-			 * @override
-			 */
-			init: function() {
-				// call the base component's init function
-				UIComponent.prototype.init.apply(this, arguments);
+      /**
+       * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+       * @public
+       * @override
+       */
+      init: function() {
+        // call the base component's init function
+        UIComponent.prototype.init.apply(this, arguments);
 
-				// enable routing
-				this.getRouter().initialize();
+        // enable routing
+        this.getRouter().initialize();
 
-				// set the device model
-				this.setModel(models.createDeviceModel(), "device");
-				debugger;
-				this.setModel(models.createHelperModel(), Constants.HELPER_MODEL);
+        // set the device model
+        this.setModel(models.createDeviceModel(), "device");
+        this.setModel(models.createHelperModel(), Constants.HELPER_MODEL);
 
-				var oInputModel = new sap.ui.model.json.JSONModel({
-					materialNumber: "",
-					busyIndicator: false
-				});
-				this.setModel(oInputModel, "inputModel");
-			}
-		});
-	}
+        var oInputModel = new sap.ui.model.json.JSONModel({
+          materialNumber: "",
+          busyIndicator: false
+        });
+        this.setModel(oInputModel, "inputModel");
+      }
+    });
+  }
 );
--- webapp/controller/MaterialDetail.controller.js
+++ webapp/controller/MaterialDetail.controller.js
@@ -1,231 +1,244 @@
 sap.ui.define(
-	[
-		"gqsystema/zmaterial_man/controller/BaseController",
-		"gqsystema/zmaterial_man/util/BusyIndicator",
-		"sap/m/MessageToast",
-		"gqsystema/zmaterial_man/util/BackendService",
-		"gqsystema/zmaterial_man/util/Constants"
-	],
-	function(
-		BaseController,
-		BusyIndicator,
-		MessageToast,
-		BackendService,
-		Constants
-	) {
-		"use strict";
-		var that;
-		return BaseController.extend(
-			"gqsystema.zmaterial_man.controller.MaterialDetail", {
-				onInit: function() {
-					that = this;
-					that.matModel = this.getModel();
-					that.backendService = new BackendService(that.matModel);
-				},
-				onSearch: function() {
+  [
+    "gqsystema/zmaterial_man/controller/BaseController",
+    "sap/m/MessageToast",
+    "gqsystema/zmaterial_man/util/BackendService",
+    "gqsystema/zmaterial_man/util/Constants"
+  ],
+  function(BaseController, MessageToast, BackendService, Constants) {
+    "use strict";
+    var that, _dialog;
+    return BaseController.extend(
+      "gqsystema.zmaterial_man.controller.MaterialDetail",
+      {
+        onInit: function() {
+          that = this;
+          that.createDialog();
+          that.matModel = this.getModel();
+          that.backendService = new BackendService(that.matModel);
+        },
+        createDialog: function() {
+          if (!that._dialog) {
+            that._dialog = sap.ui.xmlfragment(
+              "gqsystema.zmaterial_man.fragment.BusyDialog",
+              that
+            );
+            that.getView().addDependent(that._dialog);
+          }
+        },
 
-					var val = this.getView()
-						.byId("scannedValue")
-						.getProperty("value");
-					if (val) {
-						this.getModel(Constants.HELPER_MODEL).setProperty("/isLoading", true);
-						var oBusyIndicator = new BusyIndicator();
+        onSearch: function() {
+          var val = this.getView()
+            .byId("scannedValue")
+            .getProperty("value");
+          if (val) {
+            that._dialog.open();
+            var reqUrl = Constants.MATERIAL_SET + "('" + val + "')";
 
-						oBusyIndicator.show();
-						var reqUrl = Constants.MATERIAL_SET + "('" + val + "')";
-						that.backendService.readEntity(reqUrl, null, null).then(data => {
-							var layout = this.byId("bindingLayout");
-							layout.bindElement(reqUrl);
-						});
-					} else {
-						var msg = this.getModel("i18n")
-							.getResourceBundle()
-							.getText("emptyMaterialNumber");
-						MessageToast.show(msg);
-					}
-				},
-				onScanForValue: function(oEvent) {
-					if (!this._oScanDialog) {
-						this._oScanDialog = new sap.m.Dialog({
-							title: "Scan barcode",
-							contentWidth: "640px",
-							contentHeight: "480px",
-							horizontalScrolling: false,
-							verticalScrolling: false,
-							stretchOnPhone: true,
-							content: [
-								new sap.ui.core.HTML({
-									id: this.createId("scanContainer"),
-									content: "<div />"
-								})
-							],
-							endButton: new sap.m.Button({
-								text: "Cancel",
-								press: function(oEvent) {
-									this._oScanDialog.close();
-								}.bind(this)
-							}),
-							afterOpen: function() {
-								this._initQuagga(
-										this.getView()
-										.byId("scanContainer")
-										.getDomRef()
-									)
-									.done(function() {
-										// Initialisation done, start Quagga
-										Quagga.start();
-									})
-									.fail(
-										function(oError) {
-											MessageToast.error(
-												oError.message.length ? oError.message : "Failed to initialise Quagga with reason code " +
-												oError.name, {
-													onClose: function() {
-														this._oScanDialog.close();
-													}.bind(this)
-												}
-											);
-										}.bind(this)
-									);
-							}.bind(this),
-							afterClose: function() {
-								// Dialog closed, stop Quagga
-								Quagga.stop();
-							}
-						});
+            that.backendService.readEntity(reqUrl, null, null).then(data => {
+              that._dialog.close();
+              var layout = this.byId("bindingLayout");
+              layout.bindElement(reqUrl);
+            });
+          } else {
+            var msg = this.getModel("i18n")
+              .getResourceBundle()
+              .getText("emptyMaterialNumber");
+            MessageToast.show(msg);
+          }
+        },
+        onScanForValue: function(oEvent) {
+          if (!this._oScanDialog) {
+            this._oScanDialog = new sap.m.Dialog({
+              title: "Scan barcode",
+              contentWidth: "640px",
+              contentHeight: "480px",
+              horizontalScrolling: false,
+              verticalScrolling: false,
+              stretchOnPhone: true,
+              content: [
+                new sap.ui.core.HTML({
+                  id: this.createId("scanContainer"),
+                  content: "<div />"
+                })
+              ],
+              endButton: new sap.m.Button({
+                text: "Cancel",
+                press: function(oEvent) {
+                  this._oScanDialog.close();
+                }.bind(this)
+              }),
+              afterOpen: function() {
+                this._initQuagga(
+                  this.getView()
+                    .byId("scanContainer")
+                    .getDomRef()
+                )
+                  .done(function() {
+                    // Initialisation done, start Quagga
+                    Quagga.start();
+                  })
+                  .fail(
+                    function(oError) {
+                      MessageToast.error(
+                        oError.message.length
+                          ? oError.message
+                          : "Failed to initialise Quagga with reason code " +
+                              oError.name,
+                        {
+                          onClose: function() {
+                            this._oScanDialog.close();
+                          }.bind(this)
+                        }
+                      );
+                    }.bind(this)
+                  );
+              }.bind(this),
+              afterClose: function() {
+                // Dialog closed, stop Quagga
+                Quagga.stop();
+              }
+            });
 
-						this.getView().addDependent(this._oScanDialog);
-					}
+            this.getView().addDependent(this._oScanDialog);
+          }
 
-					this._oScanDialog.open();
-				},
+          this._oScanDialog.open();
+        },
 
-				_initQuagga: function(oTarget) {
-					var oDeferred = jQuery.Deferred();
-					Quagga.init({
-							inputStream: {
-								type: "LiveStream",
-								target: oTarget,
-								constraints: {
-									width: {
-										min: 640
-									},
-									height: {
-										min: 480
-									},
-									facingMode: "environment"
-								}
-							},
-							locator: {
-								patchSize: "medium",
-								halfSample: true
-							},
-							numOfWorkers: 2,
-							frequency: 10,
-							decoder: {
-								readers: [
-									"code_128_reader",
-									"ean_reader",
-									"ean_8_reader",
-									"code_39_reader",
-									"upc_reader"
-								]
-							},
-							locate: true
-						},
-						function(error) {
-							if (error) {
-								oDeferred.reject(error);
-							} else {
-								oDeferred.resolve();
-							}
-						}
-					);
+        _initQuagga: function(oTarget) {
+          var oDeferred = jQuery.Deferred();
+          Quagga.init(
+            {
+              inputStream: {
+                type: "LiveStream",
+                target: oTarget,
+                constraints: {
+                  width: {
+                    min: 640
+                  },
+                  height: {
+                    min: 480
+                  },
+                  facingMode: "environment"
+                }
+              },
+              locator: {
+                patchSize: "medium",
+                halfSample: true
+              },
+              numOfWorkers: 2,
+              frequency: 10,
+              decoder: {
+                readers: [
+                  "code_128_reader",
+                  "ean_reader",
+                  "ean_8_reader",
+                  "code_39_reader",
+                  "upc_reader"
+                ]
+              },
+              locate: true
+            },
+            function(error) {
+              if (error) {
+                oDeferred.reject(error);
+              } else {
+                oDeferred.resolve();
+              }
+            }
+          );
 
-					if (!this._oQuaggaEventHandlersAttached) {
-						// Attach event handlers...
-						Quagga.onProcessed(
-							function(result) {
-								const drawingCtx = Quagga.canvas.ctx.overlay;
-								const drawingCanvas = Quagga.canvas.dom.overlay;
-								// drawingCanvas.style.display = 'none';
+          if (!this._oQuaggaEventHandlersAttached) {
+            // Attach event handlers...
+            Quagga.onProcessed(
+              function(result) {
+                const drawingCtx = Quagga.canvas.ctx.overlay;
+                const drawingCanvas = Quagga.canvas.dom.overlay;
+                // drawingCanvas.style.display = 'none';
 
-								if (result) {
-									if (result.boxes) {
-										drawingCtx.clearRect(
-											0,
-											0,
-											parseInt(drawingCanvas.getAttribute("width"), 10),
-											parseInt(drawingCanvas.getAttribute("height"), 10)
-										);
-										result.boxes
-											.filter(box => {
-												return box !== result.box;
-											})
-											.forEach(box => {
-												Quagga.ImageDebug.drawPath(
-													box, {
-														x: 0,
-														y: 1
-													},
-													drawingCtx, {
-														color: "green",
-														lineWidth: 2
-													}
-												);
-											});
-									}
+                if (result) {
+                  if (result.boxes) {
+                    drawingCtx.clearRect(
+                      0,
+                      0,
+                      parseInt(drawingCanvas.getAttribute("width"), 10),
+                      parseInt(drawingCanvas.getAttribute("height"), 10)
+                    );
+                    result.boxes
+                      .filter(box => {
+                        return box !== result.box;
+                      })
+                      .forEach(box => {
+                        Quagga.ImageDebug.drawPath(
+                          box,
+                          {
+                            x: 0,
+                            y: 1
+                          },
+                          drawingCtx,
+                          {
+                            color: "green",
+                            lineWidth: 2
+                          }
+                        );
+                      });
+                  }
 
-									if (result.box) {
-										Quagga.ImageDebug.drawPath(
-											result.box, {
-												x: 0,
-												y: 1
-											},
-											drawingCtx, {
-												color: "#00F",
-												lineWidth: 2
-											}
-										);
-									}
+                  if (result.box) {
+                    Quagga.ImageDebug.drawPath(
+                      result.box,
+                      {
+                        x: 0,
+                        y: 1
+                      },
+                      drawingCtx,
+                      {
+                        color: "#00F",
+                        lineWidth: 2
+                      }
+                    );
+                  }
 
-									if (result.codeResult && result.codeResult.code) {
-										Quagga.ImageDebug.drawPath(
-											result.line, {
-												x: "x",
-												y: "y"
-											},
-											drawingCtx, {
-												color: "red",
-												lineWidth: 3
-											}
-										);
-									}
-								}
-							}.bind(this)
-						);
-						Quagga.onDetected(
-							function(result) {
-								// Barcode has been detected, value will be in result.codeResult.code. If requierd, validations can be done
-								// on result.codeResult.code to ensure the correct format/type of barcode value has been picked up
+                  if (result.codeResult && result.codeResult.code) {
+                    Quagga.ImageDebug.drawPath(
+                      result.line,
+                      {
+                        x: "x",
+                        y: "y"
+                      },
+                      drawingCtx,
+                      {
+                        color: "red",
+                        lineWidth: 3
+                      }
+                    );
+                  }
+                }
+              }.bind(this)
+            );
+            Quagga.onDetected(
+              function(result) {
+                // Barcode has been detected, value will be in result.codeResult.code. If requierd, validations can be done
+                // on result.codeResult.code to ensure the correct format/type of barcode value has been picked up
 
-								// Set barcode value in input field
-								this.getView()
-									.byId("scannedValue")
-									.setValue(result.codeResult.code);
+                // Set barcode value in input field
+                this.getView()
+                  .byId("scannedValue")
+                  .setValue(result.codeResult.code);
 
-								// Close dialog
-								this._oScanDialog.close();
-							}.bind(this)
-						);
+                // Close dialog
+                this._oScanDialog.close();
+              }.bind(this)
+            );
 
-						// Set flag so that event handlers are only attached once...
-						this._oQuaggaEventHandlersAttached = true;
-					}
+            // Set flag so that event handlers are only attached once...
+            this._oQuaggaEventHandlersAttached = true;
+          }
 
-					return oDeferred.promise();
-				}
-			}
-		);
-	}
+          return oDeferred.promise();
+        }
+      }
+    );
+  }
 );
