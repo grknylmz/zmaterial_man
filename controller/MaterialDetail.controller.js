ebc78b5e2a5598ae924a242fd4845f2efa8771ce
Gurkan Yilmaz <yilmaz.gurkan@hotmail.com.tr>
2019-11-06 13:43:58 +0200

    Update MaterialDetail.controller.js

--- controller/MaterialDetail.controller.js
+++ controller/MaterialDetail.controller.js
@@ -17,6 +17,70 @@ sap.ui.define(
 					that.matModel = this.getModel();
 					that.backendService = new BackendService(that.matModel);
 				},
+				onAfterRendering: function() {
+					var currView = this.getView();
+					if (window.cordova) {
+						if (window.cordova.plugins.honeywell !== undefined) {
+							window.cordova.plugins.honeywell.listConnectedBarcodeDevices(
+								function(result) {
+									result.devices.forEach(
+										function(device) {
+											console.log('name: ${device.name}, id: ${device.id}');
+										});
+								},
+								function(err) {
+									console.error(err);
+								}
+							);
+							var handler = $.proxy(function(event) {
+								var matnr;
+								var arr = event.data.split('\u001D');
+								for (var i = 0, len = arr.length; i < len; i++) {
+									if (i === 1) {
+										matnr = arr[i].substring(2);
+									}
+								}
+								if (matnr !== "") {
+									var textBox = currView.byId("scannedValue");
+									textBox.setValue(matnr);
+									this.onSearch();
+								} else {
+									console.error('Material number is empty.');
+								}
+							}, this);
+
+							window.cordova.plugins.honeywell.selectDevice('dcs.scanner.imager',
+								function() {
+									console.info('dcs.scanner.imager codebar device connected');
+									window.cordova.plugins.honeywell.claim(
+										function() {
+											console.info('claim success');
+										},
+										function(err) {
+											console.info("error claim: " + err);
+										}
+									);
+									window.cordova.plugins.honeywell.register(
+										function(event) {
+											if (event.success) {
+												handler(event);
+												console.info('new data from barcode device : ${event.data}');
+											} else {
+												console.info('scan triggered but no data');
+											}
+										},
+										function(err) {
+											console.info("error scan: " + err);
+										}
+									);
+								},
+								function(err) {
+									console.info("error select: " + err);
+								}
+							);
+						}
+					}
+				},
 				createDialog: function() {
 					if (!that._dialog) {
 						that._dialog = sap.ui.xmlfragment(
